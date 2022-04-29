86b431fdd59d04c8a49d7dd08fba231fdeec09e0
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2022-04-29 15:14:41 -0400

    changed url for netlify test

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getIGNData = async (URL) => {
     try {
-        const { data } = await axios.get(URL);
+        const { data } = await axios.get(`https://ign-apis.herokuapp.com${URL}`);
         return data;
     } catch(e) {
         console.log(e);
