5b02836e956bebca030948788ee1f1dad10e25f1
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2022-04-29 15:34:37 -0400

    configured with redirect for netlify

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getIGNData = async (URL) => {
     try {
-        const { data } = await axios.get(`https://ign-apis.herokuapp.com${URL}`);
+        const { data } = await axios.get(`/api${URL}`);
         return data;
     } catch(e) {
         console.log(e);
